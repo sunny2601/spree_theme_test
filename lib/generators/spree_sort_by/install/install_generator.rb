2f3d4c46ef87460f0a495890708f4c35e1b17ccc
sunny2601 <sunnysarrot@gmail.com>
2015-12-19 18:47:49 +0530

    Update install_generator.rb

--- lib/generators/spree_sort_by/install/install_generator.rb
+++ lib/generators/spree_sort_by/install/install_generator.rb
@@ -8,8 +8,8 @@ module SpreeVrss
       end
 
       def add_stylesheets
-        inject_into_file 'vendor/assets/stylesheets/spree/frontend/all.css', " *= require spree/frontend/spree_price_slider\n", :before => /\*\//, :verbose => true
-        inject_into_file 'vendor/assets/stylesheets/spree/backend/all.css', " *= require spree/backend/spree_price_slider\n", :before => /\*\//, :verbose => true
+       # inject_into_file 'vendor/assets/stylesheets/spree/frontend/all.css', " *= require spree/frontend/spree_price_slider\n", :before => /\*\//, :verbose => true
+      #  inject_into_file 'vendor/assets/stylesheets/spree/backend/all.css', " *= require spree/backend/spree_price_slider\n", :before => /\*\//, :verbose => true
       end
 
       def add_migrations
